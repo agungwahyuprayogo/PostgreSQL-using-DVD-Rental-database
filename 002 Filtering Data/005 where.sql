9f8719bbb97e7632924a6a9cf90e2161e8c6c090
AgungWP <101789879+agungwahyuprayogo@users.noreply.github.com>
2025-01-13 16:13:44 +0700

    005 where.sql

--- 002 Filtering Data/005 where.sql
+++ 002 Filtering Data/005 where.sql
@@ -1,20 +1,4 @@
-/* sekarang kita bakal latihan 'where' 
- * query ini digunain buat nampilin select yang lebih spesifik
- * 
- * penggambarannya seperti ini :
- * 
- * SELECT 
- * 		nama_kolom
- * FROM
- * 		nama_table
- * WHERE
- * 		kondisi
- * 
- * 
- * 
- * sekarang kita latihan menggunakan operator '='
- */*/
- 
+
  select first_name, last_name from customer c where first_name = 'Jamie'
  
 -- dari query diatas, terlihat yang tampil cuman 2, 
@@ -24,49 +8,15 @@
  -- sekarang kita latihan menggunakan operator 'AND'
  
  select first_name, last_name from customer c where first_name = 'Jamie' and last_name = 'Rice'
- 
-
-/* selain '=' dan 'and' ada banyak sebenernya
- * =
- * >
- * <
- * >=
- * <=
- * <> atau !=
- * and
- * or
- * in
- * between
- * like
- * is null
- * not
- * 
- * sekarang kita latihan make or
- * 
- */*/
- 
+  
  select first_name, last_name from customer c where last_name = 'Rodriguez' or first_name = 'Adam'
  
- /* maksud dari query diatas adalah, 
-  * tampilin orang yang nama belakangnya 'Rodriguez' atau 
-  * orang yang nama depanya 'Adam'
-  * karena 22nya ada, jadi lah ke tampil 22nya
-  * 
-  * sekarang kita ganti 'Rodriguez' jadi 'Rodigues' (make s)
-  * tetep muncul hasil yaitu adam,
-  * karena kalo make 'or' asal salah satu ada, ya tetep di tampilin
-  * 
-  * kalo mau nampilin lebih dari satu nama,
-  * bisa make 'in'
-  * 
-  * */
- 
+
  select first_name, last_name from customer c where first_name in ('Ann', 'Anne', 'Annie');
  
 /* karena ketiga nama tersebut mirip2, bisa make 'like'
  * 
- */*/
- 
+ */
  select first_name, last_name from customer c where first_name like 'Ann%'
  -- di query ini, kita ingin menampilkan dimana first name yang ada 'Ann' di depan
  -- maka hasilnya ada anna, ann, anne, annie dst
