22b316aeba90681c06c84dc0b356f75f8ce233e9
Agung Wahyu Prayogo <101789879+agungwahyuprayogo@users.noreply.github.com>
2025-05-20 11:30:19 +0700

    Update 008 limit.sql

--- 002 Filtering Data/008 limit.sql
+++ 002 Filtering Data/008 limit.sql
@@ -1,22 +1,24 @@
-# kali ini kita bakal bahas soal 'limit'
-# sesuai namanya, kita bakal batesin hasil yang keluar dari database yang pengen kita tampilin
-# kalo masukin limitnya 1, ya bakal nampilin 1 doang
-# kalo masukin limit 0? berarti sama sekali ga ada yang ditampilin
+-- kali ini kita bakal bahas soal 'limit'
+-- sesuai namanya, kita bakal batesin hasil yang keluar dari database yang pengen kita tampilin
+-- kalo masukin limitnya 1, ya bakal nampilin 1 doang
+-- kalo masukin limit 0? berarti sama sekali ga ada yang ditampilin
 
---SELECT 
---  select_list 
---FROM 
---  table_name 
---ORDER BY 
---  sort_expression 
---LIMIT 
---  row_count (jumlah baris yang pengen ditampilin)
---OFFSET 
---  row_to_skip; (jumlah baris yang mau di skip berapa nomer)
+-- 1. menampilkan film_id, title, dan release year, hanya 10 data
+select 
+	film_id, title, release_year 
+from 
+	film 
+order by 
+	film_id limit 10
+	
+-- 2. menggunakan limit dan offset 
+select 
+  film_id,
+  title,
+  release_year
+from 
+  film
+order by 
+  film_id -- urutkan berdasarkan film_id
+limit 4 offset 3; -- skip bari 1 sampai 3, makanya langsung ke 4 hasilnya
 
-select film_id, title, release_year from film order by film_id limit 10
-
-select film_id, title, release_year from film order by film_id limit 4 offset 3
-# pertama ngurutin hasil dari id (terkecil ke terbesar), 
-# kedua, skip baris dari 1 sampe 3
-# ketiga, nampilin 4 baris
