226c3c892dffe10167f3f2e259aafacedce53d46
Agung Wahyu Prayogo <101789879+agungwahyuprayogo@users.noreply.github.com>
2025-05-20 13:31:31 +0700

    Update 009 fetch perubahan di penjelasan dan first | next.sql

--- 002 Filtering Data/009 fetch.sql
+++ 002 Filtering Data/009 fetch.sql
@@ -1,12 +1,12 @@
-# kali ini kita mau belajar mengenai 'fetch'
-# jadi mirip limit sih sebenernya
-# tapi dalam standarnya, untuk menampilkan beberapa baris menggunakan fetch
-# karena limit tidak selalu di dukung oleh semua RDBMS
+-- kali ini kita mau belajar mengenai 'fetch'
+-- jadi mirip limit sih sebenernya
+-- tapi dalam standarnya, untuk menampilkan beberapa baris menggunakan fetch
+-- karena limit tidak selalu di dukung oleh semua RDBMS
 
-# di fetch ini lebih ribet tapi mayan berguna kalo sewaktu waktu ga bisa make limit
+-- di fetch ini lebih ribet tapi mayan berguna kalo sewaktu waktu ga bisa make limit
 
-# OFFSET row_to_skip { ROW | ROWS }
-# FETCH { FIRST | NEXT } [ row_count ] { ROW | ROWS } ONLY
+-- OFFSET row_to_skip { ROW | ROWS }
+-- FETCH { FIRST | NEXT } [ row_count ] { ROW | ROWS } ONLY
 
 
 
@@ -20,4 +20,10 @@ select film_id, title from film order by film_id fetch first 5 row only
 -- nampilin 5 baris paling atas
 
 select film_id, title from film order by film_id offset 5 rows fetch first 5 row only
--- skip 5 baris awal, berhenti kalo udah 5 baris
+-- skip 5 baris awal, ambil 5 baris pertama saja
+
+select film_id, title from film order by film_id offset 5 rows fetch first 10 row only
+
+--FETCH FIRST kadang digunakan di awal paginasi (ambil baris pertama).
+--FETCH NEXT sering digunakan untuk step berikutnya (paging selanjutnya), lebih semantik aja.
+-- jadi mau make first atau next hasilnya sama aja
