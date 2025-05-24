a8673ee58592f0c5e6d0fc4e90e46ccfb58291c0
Agung Wahyu Prayogo <101789879+agungwahyuprayogo@users.noreply.github.com>
2025-05-24 14:37:48 +0700

    Update and rename 16 left join.sql to 017 left join.sql

--- 003 Join Table/017 left join.sql
+++ 003 Join Table/017 left join.sql
@@ -1,8 +1,7 @@
 -- LEFT JOIN
 /* ya inti dari left join adalah
- * semua yang ada di table kiri (table 1)
- * yang ada di table 2 juga di tampilin (kalo matching di table 1 dan 2)
- * yang datanya ga ada di table 2 otomatis jadi null
+ * nampilin semua yang ada di table kiri setelah from
+ * beberapa table kanan table setelah left join ada yg null
  * 
  * biar ga pusing kita latihan left join berikut :
  * langsung metode expert aja ya biar ga kebanyakan nulis
@@ -15,20 +14,32 @@ select
 from 
 	film f 
 left join 
-	inventory i 
-using
-	(film_id) 
+	inventory i using (film_id) 
 order by 
 	title 
 	
 /* kalo kalian cek itu di film_id = 14, title = Alice Fantasia, 
  * inventory id nya ga ada di table inventory
- * maksudnya di table inventory ga ada film_id 14
- * kita cek dah yuk biar ga penasaran 
+ * karena ga ada yg nyewa film Alice Fantasia
+ * kasian bet njir
 */
+	
+-- kita cek lagi, make count
+select 
+	f.film_id,
+	f.title,
+	count(i.inventory_id) total_sewa
+from 
+	film f 
+left join 
+	inventory i using (film_id) 
+group by 1
+having count(i.inventory_id) = 0
+order by 2
+
 
 select distinct film_id from inventory i order by film_id
--- pas di cek ternyata bener, film
+-- pas di cek ternyata bener, film_id 14 ga ada
 
 -- biar ga ribet cek atu atu, kita tampilin aja yang null
 select 
