0e2a1a534e9ad9caf08cb04808a60f6f4e5aa2d3
Agung Wahyu Prayogo <101789879+agungwahyuprayogo@users.noreply.github.com>
2025-11-30 15:42:48 +0700

    Enhance SQL file with AND operator examples and comments
    Added explanations and examples for the AND operator in SQL, including boolean evaluations and usage with WHERE clause.

--- 002 Filtering Data/006 and operator.sql
+++ 002 Filtering Data/006 and operator.sql
@@ -1,35 +1,60 @@
-/* intinya sih kalo operator AND bakal ngasilin False kalo ga terpenuhi semua
- * 
+/* 
+AND digunakan untuk menggabungkan dua atau lebih kondisi. 
+Hasilnya hanya akan TRUE jika SEMUA kondisi yang digabungkan terpenuhi.
+
+bisa juga digunakan untuk menampilkan hasil boolean
+
+*/
+
+
+-------------------------------------------------------------------------------------------------------
+
+
+-- 1. Operator AND dalam boolean di postgreSQL
+
+select true and true as result -- hasil centang karena true and true = true
+
+select true and false as result -- tidak ada centak karena true and false = false
+
+select true and null as result -- karena null data kosong, tidak ada yang dibandingkan, hasilnya null
+
+----
+
+select false and true as result -- false karena salah satu false
+
+select false and false as result -- false semua
+
+select false and null as result -- jika salah satu false, tidak peduli dia null atau true, jika dibandingkan false hasilnya false
+
+---
+
+select null and null as result -- akan true jika keduanya true, akan false jika salah satu false, maka akan null jika keduanya null
+
+/* 
+ 
+ sehingga urutannya : 
+ 1. false
+ 2. null
+ 3. true
+ 
  */
 
-select true and true as result -- hasil [v]
 
-select true and false as result -- hasil [ ]
 
-select true and null as result -- hasil [NULL]
+-------------------------------------------------------------------------------------------------------
 
--------------------------------------------------
 
-select false and false as result -- hasil [ ]
 
-select false and true as result -- [ ]
 
-select false and null as result -- [ ]
+-- 2. Penggunaan operator AND dengan klausa WHERE
 
--------------------------------------------------
-
-select null and true as result -- hasil [NULL]
-
-select null and false as result -- [ ]
-
-select null and null as result -- [NULL]
-
-/* secara urutan keliatan False > NULL > True
- * 
- */
-
--- MENGGUNAKAN AND OPERATOR DENGAN WHERE
-select f.title, f.length, f.rental_rate from film f 
-
-select f.title, f.length, f.rental_rate from film f where f.length > 100 and f.rental_rate > 4
-
+-- kita akan mencari film yang durasinya lebih dari 180 menit dan tarif sewa kurang dari 1
+select 
+	title,
+	length,
+	rating
+from 
+	film
+where 
+	length > 180 and rental_rate < 1
+	
